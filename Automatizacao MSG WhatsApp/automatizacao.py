abc67c3ad371bb602e9d77b574397406b9028090
FabricioPP <fabriciopatrocinio94@hotmail.com>
2018-12-10 21:31:17 -0200

    Automatização de mensagens para wpp

---
+++ Automatizacao MSG WhatsApp/automatizacao.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import webbrowser
+
+#driver = webbrowser.open('https://web.whatsapp.com/')
+#'''C:\Users\Fabricio\Desktop\chromedriver'''
+driver = webdriver.Firefox()
+driver.get('https://web.whatsapp.com/')
+
+all_name = ['Teste', 'Mensagem', 'Ana Luyza', 'Bot', 'Sansao']
+msg = "Promoção 10% de desconto no queijo"
+count = 2
+
+input('Aperte ENTER após ler o QR Code')
+
+for name in all_name:
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+    user.click()
+
+    msg_box = driver.find_element_by_class_name('_2S1VP')
+
+    for i in range(count):
+        msg_box.send_keys(msg)
+        button = driver.find_element_by_class_name('_35EW6')
+        button.click()
